4676464112e968ebfb17024c4b17153fe2b9d14b
shutemov <shutemov1997@mail.ru>
2020-03-21 15:26:34 +0300

    Create App.vue

---
+++ client/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <todo-app></todo-app>
+    </div>
+</template>
+
+<script>
+    import todoApp from './components/TodoApp'
+
+    export default {
+        name: 'App',
+        components: {
+            todoApp,
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+</style>
