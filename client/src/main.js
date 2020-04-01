84117603572f90ecc3f455002251b535522de263
shutemov <shutemov1997@mail.ru>
2020-04-01 21:06:27 +0300

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,19 +1,27 @@
 import Vue from 'vue'
-// import Vuex from 'vuex'
+import Vuex from 'vuex'
 import App from './App.vue'
 import bulma from 'bulma'
 import store from '../src/store/store'
+import {log} from '@logux/vuex/node_modules/@logux/client'
 
 
-// Vue.use(Vuex)
+Vue.use(Vuex)
 Vue.use(bulma)
-Vue.use(store)
 
 
 Vue.config.productionTip = false
 
 
+//console log for logux app
+log(store.client)
+
 new Vue({
-  render: h => h(App),
-  store
+    store,
+    render: h => h(App)
 }).$mount('#app')
+
+// logux socket listener start
+store.client.start()
+
+
