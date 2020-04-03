30d484ebd4b766e09d3769fc957823676a2d4932
shutemov <shutemov1997@mail.ru>
2020-04-04 00:06:54 +0300

    Update main.js
    Minor structure changes

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import App from './App.vue'
+
 import bulma from 'bulma'
+import 'bulma/css/bulma.css'
+
 import store from '../src/store/store'
 import {log} from '@logux/vuex/node_modules/@logux/client'
-import 'bulma/css/bulma.css'
 
 Vue.use(Vuex)
 Vue.use(bulma)
