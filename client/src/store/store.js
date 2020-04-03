a14f3041a0449aeed8ca106f6636e26054af5671
shutemov <shutemov1997@mail.ru>
2020-04-04 00:10:01 +0300

    Update store.js
    Added comments, todoDone mutation, validator for newTodo in todoAdd action

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -24,30 +24,63 @@ export default new Logux.Store({
 
     mutations: {
 
+        /*
+        * [mutation] add todo
+        */
         todoAdd(state, action) {
             console.log('[MUTATION] addTodo', action)
-            const newTodo = action.value
-            state.todos.push(newTodo)
 
+            const newTodo = action.value
+
+            //Checking on existing
+            const todoCandidate = state.todos.find(todo => {
+                return todo.title === newTodo.title
+            })
+
+            if (!todoCandidate) {
+                state.todos.push(newTodo)
+            }
         },
 
 
+        /*
+        * [mutation] delete todo
+        */
         todoDelete(state, action) {
             console.log('[MUTATION] deleteTodo ', action)
+
             state.todos = state.todos.filter((_todo) => {
                 return _todo.title !== action.value.title
             })
-            console.log('[MUTATION] deleteTodo ', state.todos)
         },
 
+
         /*
-        * Getting init state after subscribe on the channel 'todo/all'
+        * [mutation] change state
+        */
+        todoDone(state, action) {
+            console.log('[MUTATION] todoDone', action)
+
+            const todoTitle = action.value.title
+
+            const index = state.todos.findIndex((todo) => {
+                return todo.title === todoTitle
+            })
+
+            if (index === -1) return
+
+            state.todos[index].isDone = true
+        },
+
+
+        /*
+        * [mutation] Getting init state after subscribe on the channel 'todo/all'
         */
         todoAll(state, action) {
             console.log('[MUTATION] todoAll ', action)
+
             state.todos = [...action.todos]
         },
-
     },
 
 
@@ -55,6 +88,10 @@ export default new Logux.Store({
 
 
     getters: {
+
+        /*
+        * [getter] Getting init state after subscribe on the channel 'todo/all'
+        */
         getTodos: (state) => {
             return state.todos
         }
